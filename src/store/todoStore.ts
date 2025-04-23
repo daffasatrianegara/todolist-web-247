290c5a3f7d431e1606ff1542deb15fa8e675a236
daffasatrianegara <daffasatria036@gmail.com>
2025-04-23 16:53:10 +0700

    prettier

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -1,28 +1,28 @@
-import { create } from "zustand";
-import { TodoStore } from "../types/todos.types";
+import { create } from 'zustand';
+import { TodoStore } from '../types/todos.types';
 
 const setTodoStore = create<TodoStore>((set) => ({
-    todos: [],
-    addTodo(title, description) {
-        set((state) => ({
-            todos: [...state.todos, { id: Date.now(), title, description, status: false }]
-        }))
-    },
-    updateTodo(id, title, description) {
-        set((state) => ({
-            todos: state.todos.map((t) => (t.id === id ? { ...t, title, description } : t))
-        }))
-    },
-    updateStatusTodo(id) {
-        set((state) => ({
-            todos: state.todos.map((t) => (t.id === id) ? { ...t, status: true } : t )
-        }))
-    },
-    deleteTodo(id) {
-        set((state) => ({
-            todos: state.todos.filter((t) => t.id !== id)
-        }))
-    },
-}))
+  todos: [],
+  addTodo(title, description) {
+    set((state) => ({
+      todos: [...state.todos, { id: Date.now(), title, description, status: false }],
+    }));
+  },
+  updateTodo(id, title, description) {
+    set((state) => ({
+      todos: state.todos.map((t) => (t.id === id ? { ...t, title, description } : t)),
+    }));
+  },
+  updateStatusTodo(id) {
+    set((state) => ({
+      todos: state.todos.map((t) => (t.id === id ? { ...t, status: true } : t)),
+    }));
+  },
+  deleteTodo(id) {
+    set((state) => ({
+      todos: state.todos.filter((t) => t.id !== id),
+    }));
+  },
+}));
 
-export default setTodoStore
+export default setTodoStore;
