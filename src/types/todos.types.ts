153433749b08e233104cb922a609ae2a29ddf44f
daffasatrianegara <daffasatria036@gmail.com>
2025-04-23 16:36:49 +0700

    prettier

--- src/types/todos.types.ts
+++ src/types/todos.types.ts
@@ -1,14 +1,14 @@
 export type Todos = {
-    id: number
-    title: string
-    description: string
-    status: boolean
-}
+  id: number;
+  title: string;
+  description: string;
+  status: boolean;
+};
 
 export type TodoStore = {
-    todos: Todos[]
-    addTodo: (title: string, description: string)  => void
-    updateTodo: (id: number, title: string, description: string) => void
-    updateStatusTodo: (id: number, status: boolean) => void
-    deleteTodo: (id: number) => void
-}
+  todos: Todos[];
+  addTodo: (title: string, description: string) => void;
+  updateTodo: (id: number, title: string, description: string) => void;
+  updateStatusTodo: (id: number, status: boolean) => void;
+  deleteTodo: (id: number) => void;
+};
